d7b899819a7918a4ec233c9598e67eff53bc31bb
YangBen <73685157+YangBen0528@users.noreply.github.com>
2025-08-21 16:29:39 +0800

    Update snp calling.sh

--- snp calling.sh
+++ snp calling.sh
@@ -2,7 +2,7 @@
 
 fastp -i  ${i}_1_raw.fq.gz -I ${i}_2_raw.fq.gz -o ${i}_1_out.fq.gz -O ${i}_2_out.fq.gz -h out_R.html -j out_R.json
 
-bwa mem -t 20 -R '@RG\tID:'$i'\tSM:'$i'\tPL:illumina'  genome.fasta  ${i}_1_out.fq.gz ${i}_2_out.fq.gz | samtools sort -@ 20 -m 4G  -o  ${i}.sort.bam -
+bwa mem -t 20 -R '@RG\tID:'$i'\tSM:'$i'\tPL:illumina'  genome.fa  ${i}_1_out.fq.gz ${i}_2_out.fq.gz | samtools sort -@ 20 -m 4G  -o  ${i}.sort.bam -
 
 picard -Xmx10g MarkDuplicates I=${i}.sort.bam O=${i}.sort.rmdup.bam CREATE_INDEX=true REMOVE_DUPLICATES=true   M=${i}.sort.markdup_metrics.txt
 
